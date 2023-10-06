729f90bee9ea07e1135a7b77e5d9e06e70b03935
Bram <ibramsterdam@hotmail.com>
2023-10-06 14:38:02 +0200

    auth logic with some frontend

---
+++ app/controllers/examples_controller.rb
@@ -0,0 +1,16 @@
+class ExamplesController < ApplicationController
+  skip_before_action :verify_authenticity_token, raise: false
+  before_action :authenticate_devise_api_token!, only: [:restricted]
+
+  def unrestricted
+  end
+
+  def restricted
+    devise_api_token = current_devise_api_token
+    if devise_api_token
+      render json: { message: "You are logged in as #{devise_api_token.resource_owner.email}" }, status: :ok
+    else
+      render json: { message: 'You are not logged in' }, status: :unauthorized
+    end
+  end
+end
